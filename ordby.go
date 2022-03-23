b6d9c4a2bafa691f37d535524d92ac10438e21fc
Aleksander Zaruczewski <zaruczewski@gmail.com>
2022-03-23 17:26:11 +0500

    Allow passing multiple columns to ORDER BY builder at once

--- ordby.go
+++ ordby.go
@@ -6,6 +6,7 @@ package sphinxql
 
 import (
 	"fmt"
+	"strings"
 )
 
 // OrdBy provides several helper methods to build ORDER BY clause.
@@ -15,17 +16,36 @@ func (o *OrdBy) orderBy(value string, dir string) string {
 	return fmt.Sprintf("%s%s", value, dir)
 }
 
+func (o *OrdBy) orderByMulti(values []string, dir string) string {
+	return fmt.Sprintf("%s%s", strings.Join(values, fmt.Sprintf("%s, ", dir)), dir)
+}
+
 // NoDir builds an ORDER BY clause with no direction specified.
 func (o *OrdBy) NoDir(value string) string {
 	return o.orderBy(value, "")
 }
 
+// NoDirMulti builds an ORDER BY clause with multiple columns and no direction specified.
+func (o *OrdBy) NoDirMulti(values []string) string {
+	return o.orderByMulti(values, "")
+}
+
 // Asc builds an ORDER BY ASC clause.
 func (o *OrdBy) Asc(value string) string {
-	return o.orderBy(value, " "+"ASC")
+	return o.orderBy(value, " ASC")
+}
+
+// AscMulti builds an ORDER BY ASC clause with multiple columns.
+func (o *OrdBy) AscMulti(values []string) string {
+	return o.orderByMulti(values, " ASC")
 }
 
 // Desc builds an ORDER BY DESC clause.
 func (o *OrdBy) Desc(value string) string {
-	return o.orderBy(value, " "+"DESC")
+	return o.orderBy(value, " DESC")
+}
+
+// DescMulti builds an ORDER BY DESC clause with multiple columns.
+func (o *OrdBy) DescMulti(values []string) string {
+	return o.orderByMulti(values, " DESC")
 }
